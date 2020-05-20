0dd9feb01826f8214d09257a5bfe00c97288eabb
IcedPuck <645612410@qq.com>
2020-05-20 20:01:37 +0800

    Email and Phone
    Beta Demo

--- Demo2/pages/email_and_phone_change/email_and_phone_change.js
+++ Demo2/pages/email_and_phone_change/email_and_phone_change.js
@@ -12,40 +12,40 @@ Page({
     visual: 'hidden'
   },
   //从searchBox里面获取数据
-  searchBox: function (e) {
+  searchBox: function(e) {
     const that = this
     let phone, email
     //将数据上传到EPDetail里面
     db.collection('EPDetail').where({
-
+      
     })
-      .get({
-        success: function (res) {
-          // res.data 是包含以上定义的一条记录的数组
-          console.log(res.data)
-        }
-      })
+    .get({
+      success: function(res) {
+        // res.data 是包含以上定义的一条记录的数组
+        console.log(res.data)
+      }
+    })
     db.collection('EPDetail').where({
-
+      
     })
-      .update({
-        data: {
+    .update({
+      data:{
+        email: e.detail.value.email,
+        phone: e.detail.value.phone
+      },
+      success: res => {
+        console.log(res)
+        //向上面添加数据
+        that.setData({
           email: e.detail.value.email,
-          phone: e.detail.value.phone
-        },
-        success: res => {
-          console.log(res)
-          //向上面添加数据
-          that.setData({
-            email: e.detail.value.email,
-            phone: e.detail.value.phone,
-            visual: e.detail.value.email.length ? 'show' : 'hidden'
-          })
-          wx.showToast({
-            title: 'Add Success'
-          })
-        }
-      })
+          phone: e.detail.value.phone,
+          visual: e.detail.value.email.length ? 'show' : 'hidden'
+        })
+        wx.showToast({
+          title: 'Add Success'
+        })
+      }
+    })
   }
-
+  
 })
