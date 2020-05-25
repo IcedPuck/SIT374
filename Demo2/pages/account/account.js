94ddcb13f4f8d17293ecb63d5e8f62aa3124e910
IcedPuck <645612410@qq.com>
2020-05-25 13:57:14 +0800

    account page

--- Demo2/pages/account/account.js
+++ Demo2/pages/account/account.js
@@ -1,17 +1,38 @@
 // pages/account/account.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    accountdetail: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var _this = this;
+    const db = wx.cloud.database({
+      //这个是环境ID不是环境名称
+      env: 'demo-2mweo'
+    })
+
+    //2、开始查询数据了  news对应的是集合的名称   
+    db.collection('EPDetail').where({
+      _id: '123',
+    }).get({
+      //如果查询成功的话    
+      success: res => {
+        console.log(res.data)
+        //这一步很重要，给ne赋值，没有这一步的话，前台就不会显示值      
+        this.setData({
+          accountdetail: res.data
+        })
+      }
+    })
+
 
   },
 
